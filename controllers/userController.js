79a970dfcc68b518df79c6fd6c9d0dff667052aa
ShaikhNoorAbbas <112527106+ShaikhNoorAbbas@users.noreply.github.com>
2025-12-25 20:35:08 +0530

    fixing rename issue for deployment

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-import { User } from "../Models/userModel.js";
+import { User } from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 // importing dotenv so that we can use environment variables
